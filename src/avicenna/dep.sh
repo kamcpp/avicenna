e3adf1a4e15d9e1c2b626bf0c3c7599a3cd0ff76
Kamran Amini <kam.cpp@gmail.com>
2015-06-07 14:59:46 +0430

    Pom info has been updated.

--- src/avicenna/dep.sh
+++ src/avicenna/dep.sh
@@ -1,5 +1,9 @@
-mvn javadoc:javadoc
-mvn javadoc:jar
-mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -DpomFile=avicenna-1.6.pom -Dfile=avicenna-1.6.jar
-mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -DpomFile=avicenna-1.6.pom -Dfile=avicenna-1.6-sources.jar -Dclassifier=sources
-mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -DpomFile=avicenna-1.6.pom -Dfile=avicenna-1.6-javadoc.jar -Dclassifier=javadoc
+# mvn javadoc:javadoc
+# mvn javadoc:jar
+# mvn source:jar
+
+version=1.7
+
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -DpomFile=avicenna-$version.pom -Dfile=target/avicenna-$version.jar
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -DpomFile=avicenna-$version.pom -Dfile=target/avicenna-$version-sources.jar -Dclassifier=sources
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -DpomFile=avicenna-$version.pom -Dfile=target/avicenna-$version-javadoc.jar -Dclassifier=javadoc
